52f39ecbac21f91c2b8b5da9c939cf0f57e24f0a
brojangles24 <68071853+brojangles24@users.noreply.github.com>
2025-12-08 21:24:59 -0700

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ TOKEN = os.getenv("TOKEN")
 STATE_FILE = "state.json"
 
 FOLDER_URLS = [
-    #"https://raw.githubusercontent.com/hagezi/dns-blocklists/main/controld/badware-hoster-folder.json",
+    "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/controld/badware-hoster-folder.json",
     "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/controld/spam-tlds-folder.json",
     "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/controld/spam-idns-folder.json",
     #"https://raw.githubusercontent.com/hagezi/dns-blocklists/main/controld/native-tracker-amazon-folder.json",
